311740e8731e2ad4a3819ba2327818c582e088db
ytkowshik <ytkowshik@gmail.com>
2021-02-02 23:52:14 +0530

    changes without appeal

--- duck.js
+++ duck.js
@@ -1,1 +1,25 @@
 // All js changes
+function appendMsg(event){
+    var msg = document.getElementById("txt");
+    msg.onkeypress = function(event){
+        // console.log(event.code);
+    if(event.code == "Enter"){
+        
+        var vals=msg.value;
+        var chat=document.getElementById("msgarea");
+        var sendMsg = document.createElement('div');
+        sendMsg.className='msg-send';
+        sendMsg.innerHTML=vals;
+
+        chat.insertBefore(sendMsg,null);
+
+        var recMsg = document.createElement('div');
+        recMsg.className='msg-rec';
+        recMsg.innerHTML="Quack quack";
+
+        chat.insertBefore(recMsg,null);
+
+        msg.value ='';
+        }
+    };
+};
